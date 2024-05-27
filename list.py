ec3013ee5626e859811b4ff37d98050dfcdebb3b
varunuday <150408942+varunuday@users.noreply.github.com>
2024-05-27 19:32:46 -0400

    Create list.py

---
+++ list.py
@@ -0,0 +1,563 @@
+items = [
+    {
+        "title": "The Codex of Leicester",
+        "image": "codex.png",
+        "price": "$52800000",
+        "fact": "Leonardo da Vinci's Codex is the most expensive book ever sold."
+    },
+    {
+        "title": "A Banana Taped to a Wall",
+        "image": "bananna_on_wall.png",
+        "price": "$120000",
+        "fact": "A piece of art by Italian Artist Maurizio Cattelan."
+    },
+    {
+        "title": "Microtech Gefell CVM 563 / M 7S",
+        "image": "microphone.png",
+        "price": "$6704",
+        "fact": "Fancy mic"
+    },
+    {
+        "title": "Over Yonder Cay, Bahamas",
+        "image": "penthouse.png",
+        "price": "$730000",
+        "fact": "Sleeps up to 30 people. Price to rent per week."
+    },
+    {
+        "title": "Mikasa official Volley Ball",
+        "image": "volleyball.png",
+        "price": "$100",
+        "fact": "It's the same that's used in an official match."
+    },
+    {
+        "title": "Giant Drum of WD-40",
+        "image": "wd40.jpg",
+        "price": "$1009",
+        "fact": "55 Gallons"
+    },
+    {
+        "title": "Paqui 2020 One Chip Challenge",
+        "image": "one_chip_challenge.png",
+        "price": "$40",
+        "fact": "One Extremely Hot Tortilla Chip"
+    },
+    {
+        "title": "Apple Watch Hermes",
+        "image": "apple_watch.png",
+        "price": "$1500",
+        "fact": "Apple Watch with the addition of a luxury band."
+    },
+    {
+        "title": "The Penthouse Suite",
+        "image": "hotel.png",
+        "price": "$55000",
+        "fact": "HÔTEL MARTINEZ BY HYATT, Cannes, France."
+    },
+    {
+        "title": "J.K. Rowling’s Chair",
+        "image": "harry_potter_chair.png",
+        "price": "$394000",
+        "fact": "J.K. Rowling sat on this chair when writing the first 2 Harry Potter Books."
+    },
+    {
+        "title": "Gold Backpack",
+        "image": "gold_backpack.png",
+        "price": "$1650",
+        "fact": "The gold rucksack by the Billionaire Boys Club features their trademark diamond dollar pattern."
+    },
+    {
+        "title": "The Titan Zeus",
+        "image": "titan_zeus.jpg",
+        "price": "$1600000",
+        "fact": "370-INCH, 4K, TV Titan Zeus which is bigger than an elephant."
+    },
+    {
+        "title": "Graff Diamonds Hallucination Watch",
+        "image": "graff.jpg",
+        "price": "$55000000",
+        "fact": "One of the most expensive watches ever made."
+    },
+    {
+        "title": "Air Force One",
+        "image": "air_force_one.jpeg",
+        "price": "$660000000",
+        "fact": "The official aircraft for carrying the president of the United States."
+    },
+    {
+        "title": "Buckingham Palace",
+        "image": "buckingham.jpg",
+        "price": "$1550000000",
+        "fact": "Buckingham Palace is a royal residence in London."
+    },
+    {
+        "title": "Bugatti La Voiture Noire",
+        "image": "la_voiture_noire.jpeg",
+        "price": "$13400000",
+        "fact": "Bugatti La Voiture Noire was built to celebrate Bugatti's 110-year anniversary."
+    },
+    {
+        "title": "Ferrari LaFerrari",
+        "image": "laferrari.jpg",
+        "price": "$1400000",
+        "fact": "The LaFerrari is Ferrari's first ever production car to be equipped with the F1-derived hybrid solution."
+    },
+    {
+        "title": "Amazing Fantasy 15",
+        "image": "amazing_spiderman.jpg",
+        "price": "$1100000",
+        "fact": "The First Appearance of the Amazing Spider-Man!"
+    },
+    {
+        "title": "Airbus H225 Super Puma",
+        "image": "airbus.JPG",
+        "price": "$25000000",
+        "fact": "The Airbus Helicopters H225 is a long-range passenger transport helicopter."
+    },
+    {
+        "title": "Shimansky Soccer Ball",
+        "image": "soccer_ball.jpg",
+        "price": "$2590000",
+        "fact": "The one-of-a-kind Shimansky diamond soccer ball."
+    },
+    {
+        "title": "Golden Gate Bridge",
+        "image": "ggb.jpg",
+        "price": "$523000000",
+        "fact": "Bridge between West San Francisco and Sausalito."
+    },
+    {
+        "title": "Airbus A380 custom",
+        "image": "airbus_a380.jpg",
+        "price": "$402000000",
+        "fact": "Prince Al-Waleed bin Talal owns it."
+    },
+    {
+        "title": "business.com domain",
+        "image": "business.jpg",
+        "price": "$350000000",
+        "fact": "RH Donnelley bought this domain in 2007."
+    },
+    {
+        "title": "Royale Luxury Chess Set",
+        "image": "chess.jpg",
+        "price": "$9800000",
+        "fact": "The king piece alone uses 146 diamonds and 73 rubies studded in 165.2 grams of 18 carat gold."
+    },
+    {
+        "title": "Beats X",
+        "image": "beatsx.png",
+        "price": "$100",
+        "fact": "Made by Dr. Dre, and featuring the H1 Bluetooth Chip."
+    },
+    {
+        "title": "Sims 4 Electronic Copy",
+        "image": "sims4.png",
+        "price": "$40",
+        "fact": "Massively popular simulation game. PC version on Origin."
+    },
+    {
+        "title": "Xbox Series X",
+        "image": "xbox.png",
+        "price": "$499",
+        "fact": "The Latest Xbox."
+    },
+    {
+        "title": "Louis Vuitton Earbuds",
+        "image": "lv_earbuds.jpg",
+        "price": "$1035",
+        "fact": "One of the most expensive earbuds."
+    },
+    {
+        "title": "1st Edition Charizard",
+        "image": "charizard.jpg",
+        "price": "$183812",
+        "fact": "The price is the ending bid placed on this card."
+    },
+    {
+        "title": "GEFORCE RTX 3090",
+        "image": "rtx.jpg",
+        "price": "$1500",
+        "fact": "Top-of-the-line graphics card made by Nvidia."
+    },
+    {
+        "title": "X-Men First Issue Comic Book",
+        "image": "xmen.jpg",
+        "price": "$492937",
+        "fact": "The X-Men are a superhero team appearing in American comic books published by Marvel Comics."
+    },
+    {
+        "title": "Babe Ruth Autographed Baseball",
+        "image": "babe_ruth.jpg",
+        "price": "$388375",
+        "fact": "A baseball signed by The Babe."
+    },
+    {
+        "title": "G4 Folding E-Bike",
+        "image": "gocycle.jpg",
+        "price": "$4000",
+        "fact": "G4 foldable e-bike combines superb engineering and performance."
+    },
+    {
+        "title": "Nissan Skyline R34",
+        "image": "nissan_skyline.jpg",
+        "price": "$14742",
+        "fact": "Sports car produced from 1999-2002."
+    },
+    {
+        "title": "Lady Gaga's Tea Cup and Saucer",
+        "image": "lady_gaga.jpg",
+        "price": "$75137",
+        "fact": "Just a teacup with Lady Gaga's lipstick kiss."
+    },
+    {
+        "title": "Batman First Issue Solo Series Comic Book",
+        "image": "batman.jpg",
+        "price": "$567625",
+        "fact": "Batman #1 is the first issue of the Batman comic book series."
+    },
+    {
+        "title": "Penny from 1943",
+        "image": "penny.jpg",
+        "price": "$200000",
+        "fact": "Only 40 copper one cent pennies are still known to be around."
+    },
+    {
+        "title": "Porsche GT3 RS",
+        "image": "porche.jpg",
+        "price": "$187500",
+        "fact": "Has 502 hp and 4.0-liter six-cylinder horizontally opposed high-revving naturally aspirated engine."
+    },
+    {
+        "title": "GTX Titan Z",
+        "image": "titan.jpg",
+        "price": "$3000",
+        "fact": "GeForce GTX TITAN Z combines two graphics processors to increase performance."
+    },
+    {
+        "title": "Trek mountain bike",
+        "image": "trek.png",
+        "price": "$14000",
+        "fact": "Trek mountain bike is loaded with features and details that will make any ride better."
+    },
+    {
+        "title": "John Lennon’s Toilet",
+        "image": "toliet.jpg",
+        "price": "$15000",
+        "fact": "A flowered porcelain toilet."
+    },
+    {
+        "title": "AirPods Pro",
+        "image": "airpods.jpg",
+        "price": "$249",
+        "fact": "High-end earbuds by Apple."
+    },
+    {
+        "title": "Posh Instant Noodles",
+        "image": "noodles.jpeg",
+        "price": "$43",
+        "fact": "Fancy Noodles."
+    },
+    {
+        "title": "Real Life Minecraft Diamond Sword",
+        "image": "mc_sword.jpg",
+        "price": "$18",
+        "fact": "From the official Minecraft store."
+    },
+    {
+        "title": "Gillette ProGlide Razor",
+        "image": "razor.jpg",
+        "price": "$16",
+        "fact": "ProGlide features 5 antifriction razor blades and FlexBall technology."
+    },
+    {
+        "title": "GAN Rubik's Cube",
+        "image": "gan.jpg",
+        "price": "$30",
+        "fact": "Rubik's cube."
+    },
+    {
+        "title": "PlayStation 5",
+        "image": "ps5.jpg",
+        "price": "$640",
+        "fact": "The PlayStation 5 is an innovative and powerful gaming console."
+    },
+    {
+        "title": "Costco Executive Membership",
+        "image": "costco.png",
+        "price": "$120",
+        "fact": "Annual renewal for executive membership."
+    }
+]
+
+luxury = [
+    {
+        "title": "The Penthouse Suite",
+        "image": "hotel.png",
+        "price": "$55000",
+        "fact": "HÔTEL MARTINEZ BY HYATT, Cannes, France."
+    },
+    {
+        "title": "Gold Backpack",
+        "image": "gold_backpack.png",
+        "price": "$1650",
+        "fact": "The gold rucksack by the Billionaire Boys Club features their trademark diamond dollar pattern."
+    },
+    {
+        "title": "The Titan Zeus",
+        "image": "titan_zeus.jpg",
+        "price": "$1600000",
+        "fact": "370-INCH, 4K, TV Titan Zeus which is bigger than an elephant."
+    },
+    {
+        "title": "Graff Diamonds Hallucination Watch",
+        "image": "graff.jpg",
+        "price": "$55000000",
+        "fact": "One of the most expensive watches ever made."
+    },
+    {
+        "title": "Air Force One",
+        "image": "air_force_one.jpeg",
+        "price": "$660000000",
+        "fact": "The official aircraft for carrying the president of the United States."
+    },
+    {
+        "title": "Buckingham Palace",
+        "image": "buckingham.jpg",
+        "price": "$1550000000",
+        "fact": "Buckingham Palace is a royal residence in London."
+    },
+    {
+        "title": "Bugatti La Voiture Noire",
+        "image": "la_voiture_noire.jpeg",
+        "price": "$13400000",
+        "fact": "Bugatti La Voiture Noire was built to celebrate Bugatti's 110-year anniversary."
+    },
+    {
+        "title": "Ferrari LaFerrari",
+        "image": "laferrari.jpg",
+        "price": "$1400000",
+        "fact": "The LaFerrari is Ferrari's first ever production car to be equipped with the F1-derived hybrid solution."
+    },
+    {
+        "title": "Amazing Fantasy 15",
+        "image": "amazing_spiderman.jpg",
+        "price": "$1100000",
+        "fact": "The First Appearance of the Amazing Spider-Man!"
+    },
+    {
+        "title": "Airbus H225 Super Puma",
+        "image": "airbus.JPG",
+        "price": "$25000000",
+        "fact": "The Airbus Helicopters H225 is a long-range passenger transport helicopter."
+    },
+    {
+        "title": "The Codex of Leicester",
+        "image": "codex.png",
+        "price": "$52800000",
+        "fact": "Leonardo da Vinci's Codex is the most expensive book ever sold."
+    },
+    {
+        "title": "Shimansky Soccer Ball",
+        "image": "soccer_ball.jpg",
+        "price": "$2590000",
+        "fact": "The one-of-a-kind Shimansky diamond soccer ball."
+    },
+    {
+        "title": "Golden Gate Bridge",
+        "image": "ggb.jpg",
+        "price": "$523000000",
+        "fact": "Bridge between West San Francisco and Sausalito. Known for marking the entrance to the San Francisco Bay."
+    },
+    {
+        "title": "Airbus A380 custom",
+        "image": "airbus_a380.jpg",
+        "price": "$402000000",
+        "fact": "Prince Al-Waleed bin Talal owns it."
+    },
+    {
+        "title": "business.com domain",
+        "image": "business.jpg",
+        "price": "$350000000",
+        "fact": "RH Donnelley bought this domain in 2007."
+    },
+    {
+        "title": "Royale Luxury Chess Set",
+        "image": "chess.jpg",
+        "price": "$9800000",
+        "fact": "The king piece alone uses 146 diamonds and 73 rubies studded in 165.2 grams of 18 carat gold."
+    }
+]
+
+entertainment = [
+    {
+        "title": "The Penthouse Suite",
+        "image": "hotel.png",
+        "price": "$55000",
+        "fact": "HÔTEL MARTINEZ BY HYATT, Cannes, France."
+    },
+    {
+        "title": "Beats X",
+        "image": "beatsx.png",
+        "price": "$100",
+        "fact": "Made by Dr. Dre, and featuring the H1 Bluetooth Chip."
+    },
+    {
+        "title": "Sims 4 Electronic Copy",
+        "image": "sims4.png",
+        "price": "$40",
+        "fact": "Massively popular simulation game. PC version on Origin."
+    },
+    {
+        "title": "Xbox Series X",
+        "image": "xbox.png",
+        "price": "$499",
+        "fact": "The Latest Xbox."
+    },
+    {
+        "title": "Louis Vuitton Earbuds",
+        "image": "lv_earbuds.jpg",
+        "price": "$1035",
+        "fact": "One of the most expensive earbuds."
+    },
+    {
+        "title": "1st Edition Charizard",
+        "image": "charizard.jpg",
+        "price": "$183812",
+        "fact": "The price is the ending bid placed on this card."
+    },
+    {
+        "title": "GEFORCE RTX 3090",
+        "image": "rtx.jpg",
+        "price": "$1500",
+        "fact": "Top-of-the-line graphics card made by Nvidia."
+    },
+    {
+        "title": "X-Men First Issue Comic Book",
+        "image": "xmen.jpg",
+        "price": "$492937",
+        "fact": "The X-Men are a superhero team appearing in American comic books published by Marvel Comics."
+    },
+    {
+        "title": "Babe Ruth Autographed Baseball",
+        "image": "babe_ruth.jpg",
+        "price": "$388375",
+        "fact": "A baseball signed by The Babe."
+    },
+    {
+        "title": "G4 Folding E-Bike",
+        "image": "gocycle.jpg",
+        "price": "$4000",
+        "fact": "G4 foldable e-bike combines superb engineering and performance."
+    },
+    {
+        "title": "Nissan Skyline R34",
+        "image": "nissan_skyline.jpg",
+        "price": "$14742",
+        "fact": "Sports car produced from 1999-2002."
+    },
+    {
+        "title": "Lady Gaga's Tea Cup and Saucer",
+        "image": "lady_gaga.jpg",
+        "price": "$75137",
+        "fact": "Just a teacup with Lady Gaga's lipstick kiss."
+    },
+    {
+        "title": "Batman First Issue Solo Series Comic Book",
+        "image": "batman.jpg",
+        "price": "$567625",
+        "fact": "Batman #1 is the first issue of the Batman comic book series."
+    },
+    {
+        "title": "Penny from 1943",
+        "image": "penny.jpg",
+        "price": "$200000",
+        "fact": "Only 40 copper one cent pennies are still known to be around."
+    },
+    {
+        "title": "Porsche GT3 RS",
+        "image": "porche.jpg",
+        "price": "$187500",
+        "fact": "Has 502 hp and 4.0-liter six-cylinder horizontally opposed high-revving naturally aspirated engine."
+    },
+    {
+        "title": "GTX Titan Z",
+        "image": "titan.jpg",
+        "price": "$3000",
+        "fact": "GeForce GTX TITAN Z combines two graphics processors to increase performance."
+    },
+    {
+        "title": "Trek mountain bike",
+        "image": "trek.png",
+        "price": "$14000",
+        "fact": "Trek mountain bike is loaded with features and details that will make any ride, on any trail, better."
+    }
+]
+
+everyday = [
+    {
+        "title": "Paqui 2020 One Chip Challenge",
+        "image": "one_chip_challenge.png",
+        "price": "$40",
+        "fact": "One Extremely Hot Tortilla Chip."
+    },
+    {
+        "title": "John Lennon’s Toilet",
+        "image": "toliet.jpg",
+        "price": "$15000",
+        "fact": "A flowered porcelain toilet."
+    },
+    {
+        "title": "AirPods Pro",
+        "image": "airpods.jpg",
+        "price": "$249",
+        "fact": "High-end earbuds by Apple."
+    },
+    {
+        "title": "Posh Instant Noodles",
+        "image": "noodles.jpeg",
+        "price": "$43",
+        "fact": "Fancy Noodles."
+    },
+    {
+        "title": "Mikasa official Volleyball",
+        "image": "volleyball.png",
+        "price": "$100",
+        "fact": "It's the same that's used in an official match."
+    },
+    {
+        "title": "Real Life Minecraft Diamond Sword",
+        "image": "mc_sword.jpg",
+        "price": "$18",
+        "fact": "From the official Minecraft store."
+    },
+    {
+        "title": "Xbox Series X",
+        "image": "xbox.png",
+        "price": "$499",
+        "fact": "The Latest Xbox."
+    },
+    {
+        "title": "Gillette ProGlide Razor",
+        "image": "razor.jpg",
+        "price": "$16",
+        "fact": "ProGlide features 5 antifriction razor blades and FlexBall technology."
+    },
+    {
+        "title": "GAN Rubik's Cube",
+        "image": "gan.jpg",
+        "price": "$30",
+        "fact": "Rubik's cube."
+    },
+    {
+        "title": "PlayStation 5",
+        "image": "ps5.jpg",
+        "price": "$640",
+        "fact": "The PlayStation 5 is an innovative and powerful gaming console."
+    },
+    {
+        "title": "Costco Executive Membership",
+        "image": "costco.png",
+        "price": "$120",
+        "fact": "Annual renewal for executive membership."
+    }
+]
